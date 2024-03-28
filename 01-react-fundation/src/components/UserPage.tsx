0911656f92ba44c05a2e7524e1d88616e2ebd265
Mauri <mauri.monzon91@gmail.com>
2024-03-27 22:18:05 -0300

    userUsers

--- 01-react-fundation/src/components/UserPage.tsx
+++ 01-react-fundation/src/components/UserPage.tsx
@@ -1,50 +1,8 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-import type { ReqResUserListResponse, User } from "../interfaces";
-
-const loadUsers = async (page: number = 1): Promise<User[]> => {
-  try {
-    const { data } = await axios.get<ReqResUserListResponse>(
-      "https://reqres.in/api/users",
-      {
-        params: {
-          page: page,
-        },
-      }
-    );
-    return data.data;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
-};
+import { useUsers } from "../hooks/useUsers";
+import { UserRow } from "./UserRow";
 
 export const UserPage = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const currentPageRef = useRef(1);
-
-  useEffect(() => {
-    loadUsers(currentPageRef.current).then((users) => setUsers(users));
-  }, []);
-
-  const nextPage = async () => {
-    currentPageRef.current++;
-    const users = await loadUsers(currentPageRef.current);
-    if (users.length > 0) {
-      setUsers(users);
-    } else {
-      currentPageRef.current--;
-    }
-  };
-
-  const prevPage = async () => {
-    if (currentPageRef.current < 1) return;
-
-    currentPageRef.current--;
-    const users = await loadUsers(currentPageRef.current);
-    setUsers(users);
-  };
-
+  const { nextPage, prevPage, users } = useUsers();
   return (
     <>
       <h3>Usuarios:</h3>
@@ -67,24 +25,5 @@ export const UserPage = () => {
         <button onClick={nextPage}>Next</button>
       </div>
     </>
-  );
-};
-
-interface Props {
-  user: User;
-}
-
-export const UserRow = ({ user }: Props) => {
-  const { avatar, email, first_name, last_name } = user;
-  return (
-    <tr>
-      <td>
-        <img style={{ width: "50px" }} src={avatar} alt="User avatar" />
-      </td>
-      <td>
-        {first_name} {last_name}
-      </td>
-      <td>{email}</td>
-    </tr>
   );
 };
