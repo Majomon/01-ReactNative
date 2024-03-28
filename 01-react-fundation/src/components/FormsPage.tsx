9f043e8f8431f7b17de5904216ed9eb74eea3266
Mauri <mauri.monzon91@gmail.com>
2024-03-27 22:35:51 -0300

    Renderizado

--- 01-react-fundation/src/components/FormsPage.tsx
+++ 01-react-fundation/src/components/FormsPage.tsx
@@ -6,22 +6,34 @@ type FormInputs = {
 };
 
 export const FormsPage = () => {
-  const { register } = useForm<FormInputs>({
+  const { register, handleSubmit, formState, watch } = useForm<FormInputs>({
     defaultValues: {
       email: "mauri@mauri.com",
       password: "ABC123",
     },
   });
+
+  const onSubmit = (myForm: FormInputs) => {
+    console.log(myForm);
+  };
+
+  console.log(watch("email"));
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h3>Formularios</h3>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <input type="text" placeholder="Email" {...register("email")} />
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
           <input type="text" placeholder="Password" {...register("password")} />
           <button>Ingresar</button>
         </div>
       </form>
+      <pre>{JSON.stringify(formState, null, 2)}</pre>
     </>
   );
 };
