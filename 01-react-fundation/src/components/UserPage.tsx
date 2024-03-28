bc92cf1a126b2f2ac899f196c0296c1dd3d42734
Mauri <mauri.monzon91@gmail.com>
2024-03-27 21:49:41 -0300

    renderizado de usuarios

--- 01-react-fundation/src/components/UserPage.tsx
+++ 01-react-fundation/src/components/UserPage.tsx
@@ -1,27 +1,24 @@
-import { useEffect } from "react";
 import axios from "axios";
-import { ReqResUserListResponse } from "../interfaces";
-export const UserPage = () => {
-  /*   useEffect(() => {
-    fetch("https://reqres.in/api/users?page=2")
-      .then((resp) => resp.json())
-      .then((data) => console.log(data));
-  }, []); */
+import { useEffect, useState } from "react";
+import type { ReqResUserListResponse, User } from "../interfaces";
 
-  const loadUsers = async () => {
-    try {
-      const { data } = await axios.get<ReqResUserListResponse>(
-        "https://reqres.in/api/users"
-      );
-      return data.data;
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  };
+const loadUsers = async (): Promise<User[]> => {
+  try {
+    const { data } = await axios.get<ReqResUserListResponse>(
+      "https://reqres.in/api/users"
+    );
+    return data.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const UserPage = () => {
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    loadUsers().then((users) => console.log(users));
+    loadUsers().then((users) => setUsers(users));
   }, []);
 
   return (
@@ -36,13 +33,30 @@ export const UserPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>avatar</td>
-            <td>nombre</td>
-            <td>email</td>
-          </tr>
+          {users.map((user) => (
+            <UserRow key={user.id} user={user} />
+          ))}
         </tbody>
       </table>
     </>
+  );
+};
+
+interface Props {
+  user: User;
+}
+
+export const UserRow = ({ user }: Props) => {
+  const { avatar, email, first_name, last_name } = user;
+  return (
+    <tr>
+      <td>
+        <img style={{ width: "50px" }} src={avatar} alt="User avatar" />
+      </td>
+      <td>
+        {first_name} {last_name}
+      </td>
+      <td>{email}</td>
+    </tr>
   );
 };
